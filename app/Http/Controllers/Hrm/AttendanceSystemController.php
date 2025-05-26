8522887cf9ae012a66637787f0f98f75d9a91dda
zulfi <m.zulfi@duakelinci.co.id>
2025-05-26 14:22:37 +0700

    Update AttendanceSystemController.php

--- app/Http/Controllers/Hrm/AttendanceSystemController.php
+++ app/Http/Controllers/Hrm/AttendanceSystemController.php
@@ -205,77 +205,86 @@ class AttendanceSystemController extends Controller
                     $location = 'Shine Barrier';
                 }
 
-                $url = 'https://developer.fingerspot.io/api/get_attlog';
-                $randKey = bin2hex(random_bytes(25));
-                $request = '{"trans_id":"' . $randKey . '", "cloud_id":"' . $cloudId . '", "start_date":"' . $startDate . '", "end_date":"' . $endDate . '"}';
-                $authorization = "Authorization: Bearer " . $bearerId;
+                $start = new \DateTime($startDate);
+                $end   = new \DateTime($endDate);
+                $end->modify('+1 day'); // supaya end date ikut termasuk dalam loop
 
-                $ch = curl_init($url);
-                curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-                curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-                curl_setopt($ch, CURLOPT_POST, 1);
-                curl_setopt($ch, CURLOPT_POSTFIELDS, $request);
-                curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json', $authorization));
-                curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-                curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-                $response = curl_exec($ch);
-                curl_close($ch);
-                $data = ['unique_id' => $randKey, 'type' => 'get_attlog', 'request' => $request, 'response' => $response, 'created_at' => date('Y-m-d H:i:s')];
-                DB::table('finger_logs')->insert($data);
+                $interval = new \DateInterval('P1D'); // P1D = 1 day
+                $period   = new \DatePeriod($start, $interval, $end);
 
-                //import to system
-                $data = (array) json_decode($response);
-                if ($data['success']) {
-                    $listAttendance = $data['data'];
-                    if (!empty($listAttendance)) {
-                        foreach ($listAttendance as $r => $v) {
-                            $v = (array) $v;
-                            $mechanic = Mechanic::where(['id_finger' => $v['pin']])->first();
-                            if (isset($mechanic)) {
-                                $date = strtotime($v['scan_date']);
-                                $statusCheck = date('H:i:s', $date) < '12:00:00' ? 'in' : 'out';
+                foreach ($period as $date) {
+                    $url = 'https://developer.fingerspot.io/api/get_attlog';
+                    $randKey = bin2hex(random_bytes(25));
+                    $request = '{"trans_id":"' . $randKey . '", "cloud_id":"' . $cloudId . '", "start_date":"' . $date->format('Y-m-d') . '", "end_date":"' . $date->format('Y-m-d') . '"}';
+                    $authorization = "Authorization: Bearer " . $bearerId;
 
-                                //delete old data
-                                $whereArray = ['date' => date('Y-m-d'), 'type' => 'finger', 'employee_id' => $mechanic->id, 'status' => $statusCheck];
-                                $query = DB::table('attendances');
-                                foreach ($whereArray as $field => $value) {
-                                    $query->where($field, $value);
-                                }
-                                $query->delete();
+                    $ch = curl_init($url);
+                    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+                    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+                    curl_setopt($ch, CURLOPT_POST, 1);
+                    curl_setopt($ch, CURLOPT_POSTFIELDS, $request);
+                    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json', $authorization));
+                    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+                    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+                    $response = curl_exec($ch);
+                    curl_close($ch);
+                    $data = ['unique_id' => $randKey, 'type' => 'get_attlog', 'request' => $request, 'response' => $response, 'created_at' => date('Y-m-d H:i:s')];
+                    DB::table('finger_logs')->insert($data);
 
-                                //add new data
-                                $model = new Attendance();
-                                $model->location = $location;
-                                $model->employee_id = $mechanic->id;
-                                $model->finger_id = $v['pin'];
-                                $model->date = date('Y-m-d', $date);
-                                $model->time = date('H:i:s', $date);
-                                $model->status = $statusCheck;
-                                $type = "";
-                                if ($v['verify'] == 1) {
-                                    $type = 'finger';
-                                } elseif ($v['verify'] == 2) {
-                                    $type = 'password';
-                                } elseif ($v['verify'] == 3) {
-                                    $type = 'password';
-                                } elseif ($v['verify'] == 4) {
-                                    $type = 'card';
-                                } elseif ($v['verify'] == 5) {
-                                    $type = 'gps';
-                                } elseif ($v['verify'] == 6) {
-                                    $type = 'vein';
-                                }
-                                $model->type = $type;
-                                if (!$model->save()) {
-                                    $success = false;
-                                    $message = "Save Failed";
+                    //import to system
+                    $data = (array) json_decode($response);
+                    if ($data['success']) {
+                        $listAttendance = $data['data'];
+                        if (!empty($listAttendance)) {
+                            foreach ($listAttendance as $r => $v) {
+                                $v = (array) $v;
+                                $mechanic = Mechanic::where(['id_finger' => $v['pin']])->first();
+                                if (isset($mechanic)) {
+                                    $date = strtotime($v['scan_date']);
+                                    $statusCheck = date('H:i:s', $date) < '12:00:00' ? 'in' : 'out';
+
+                                    //delete old data
+                                    $whereArray = ['date' => date('Y-m-d'), 'type' => 'finger', 'employee_id' => $mechanic->id, 'status' => $statusCheck];
+                                    $query = DB::table('attendances');
+                                    foreach ($whereArray as $field => $value) {
+                                        $query->where($field, $value);
+                                    }
+                                    $query->delete();
+
+                                    //add new data
+                                    $model = new Attendance();
+                                    $model->location = $location;
+                                    $model->employee_id = $mechanic->id;
+                                    $model->finger_id = $v['pin'];
+                                    $model->date = date('Y-m-d', $date);
+                                    $model->time = date('H:i:s', $date);
+                                    $model->status = $statusCheck;
+                                    $type = "";
+                                    if ($v['verify'] == 1) {
+                                        $type = 'finger';
+                                    } elseif ($v['verify'] == 2) {
+                                        $type = 'password';
+                                    } elseif ($v['verify'] == 3) {
+                                        $type = 'password';
+                                    } elseif ($v['verify'] == 4) {
+                                        $type = 'card';
+                                    } elseif ($v['verify'] == 5) {
+                                        $type = 'gps';
+                                    } elseif ($v['verify'] == 6) {
+                                        $type = 'vein';
+                                    }
+                                    $model->type = $type;
+                                    if (!$model->save()) {
+                                        $success = false;
+                                        $message = "Save Failed";
+                                    }
                                 }
                             }
                         }
+                    } else {
+                        $success = false;
+                        $message = "Attendance empty or API error: " . $data['message'];
                     }
-                } else {
-                    $success = false;
-                    $message = "Attendance empty or API error: " . $data['message'];
                 }
             }
         } else {
